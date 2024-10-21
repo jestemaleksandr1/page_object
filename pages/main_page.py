b5c82d0eb1e82a576395b4a72ba7953b5842c3a0
Aleksandr Alymov <veder31@gmail.com>
2024-10-21 13:56:47 -0500

    Files updated due to lesson 4.2.12

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,17 @@
 from .base_page import BasePage
 from .locators import MainPageLocators
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class MainPage(BasePage):
     def go_to_login_page(self):
         link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         link.click()
+        
+        # Ожидание появления alert (до 5 секунд)
+        try:
+            WebDriverWait(self.browser, 5).until(EC.alert_is_present())
+            alert = self.browser.switch_to.alert
+            alert.accept()
+        except:
+            print("No alert appeared after clicking login link")
