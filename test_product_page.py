ea8d82b47b0f484dacc4be3be2215b11d55d06b4
Aleksandr Alymov <veder31@gmail.com>
2024-10-21 14:41:08 -0500

    Organized tests into classes and added setup fixture for product page tests

--- test_product_page.py
+++ test_product_page.py
@@ -1,11 +1,24 @@
+import pytest
 from .pages.product_page import ProductPage
-from .pages.basket_page import BasketPage
 
-def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.go_to_basket()
-    basket_page = BasketPage(browser, browser.current_url)
-    basket_page.should_not_be_items_in_basket()
-    basket_page.should_be_empty_basket_message()
+@pytest.mark.login
+class TestLoginFromProductPage():
+    
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self):
+        # В реальном проекте можно было бы создать товар через API
+        # self.product = ProductFactory(title="Best book created by robot")
+        self.link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/"
+        yield
+        # После выполнения теста можно было бы удалить данные
+        # self.product.delete()
+
+    def test_guest_can_go_to_login_page_from_product_page(self, browser):
+        page = ProductPage(browser, self.link)
+        page.open()
+        page.go_to_login_page()
+
+    def test_guest_should_see_login_link(self, browser):
+        page = ProductPage(browser, self.link)
+        page.open()
+        page.should_be_login_link()
