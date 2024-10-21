2cdee1efdb5c091600454630e2072f0d9747c631
Aleksandr Alymov <veder31@gmail.com>
2024-10-21 14:12:41 -0500

    Mark test with offer7 as xfail due to known bug

--- pages/locators.py
+++ pages/locators.py
@@ -2,7 +2,14 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    
+class ProductPageLocators():
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")  # Кнопка "Добавить в корзину"
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alert-success .alertinner strong")  # Сообщение о добавлении товара
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")  # Название продукта
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")  # Цена продукта
+    BASKET_TOTAL = (By.CSS_SELECTOR, ".basket-mini")  # Общая сумма корзины
 
-class LoginPageLocators():
+class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+from .pages.product_page import ProductPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com"
@@ -8,3 +9,25 @@ def test_guest_can_go_to_login_page(browser):
     page.go_to_login_page()  # Переходим на страницу логина
     login_page = LoginPage(browser, browser.current_url)  # Явная инициализация страницы логина
     login_page.should_be_login_page()  # Проверяем, что это действительно страница логина
+    
+def test_guest_can_add_product_to_basket(browser):
+    # Указываем ссылку на страницу с продуктом и промо-акцией
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    
+    # Инициализируем страницу продукта
+    page = ProductPage(browser, link)
+    
+    # Открываем страницу продукта
+    page.open()
+    
+    # Добавляем товар в корзину
+    page.add_to_basket()
+    
+    # Решаем математическую задачу и подтверждаем
+    page.solve_quiz_and_get_code()
+    
+    # Проверяем, что появилось сообщение о добавлении товара в корзину
+    page.should_be_success_message()
+    
+    # Проверяем, что сумма корзины соответствует цене товара
+    page.should_be_basket_total()
