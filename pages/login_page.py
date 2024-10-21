594a64e12be8949c6763c3dd4b2c99da575e24e5
Aleksandr Alymov <veder31@gmail.com>
2024-10-21 13:32:36 -0500

    New files added for the 4.2.8 task

---
+++ pages/login_page.py
@@ -0,0 +1,20 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # Проверка, что подстрока "login" есть в URL
+        assert "login" in self.browser.current_url, "Login URL is incorrect"
+
+    def should_be_login_form(self):
+        # Проверка, что форма логина присутствует на странице
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
+    def should_be_register_form(self):
+        # Проверка, что форма регистрации присутствует на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,14 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from .locators import MainPageLocators
 
 class MainPage(BasePage): 
+    # Метод для перехода на страницу логина
     def go_to_login_page(self):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
+
+    # Метод для проверки наличия ссылки на логин
+    def should_be_login_link(self):
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
